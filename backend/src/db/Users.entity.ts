dcf9901d6f4250e86362196006c071682a6f4af1
11ai <124818466+11ai@users.noreply.github.com>
2024-02-10 11:28:00 +0200

    edited on Sat Feb 10 2024 11:27:24 GMT+0200 (Eastern European Standard Time)

--- backend/src/db/Users.entity.ts
+++ backend/src/db/Users.entity.ts
@@ -6,8 +6,8 @@ export class UsersEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column("integer",{nullable: true})
-userId: number;
+  @Column("text",{nullable: true})
+userId: string;
 
 @Column("text",{nullable: true})
 email: string;
