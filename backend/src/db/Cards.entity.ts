0977c76e10efe11988ae2542f7cacd2e6b8433bd
11ai <124818466+11ai@users.noreply.github.com>
2024-02-10 11:34:43 +0200

    edited on Sat Feb 10 2024 11:34:11 GMT+0200 (Eastern European Standard Time)

--- backend/src/db/Cards.entity.ts
+++ backend/src/db/Cards.entity.ts
@@ -15,8 +15,8 @@ setid: number;
 @Column("text",{nullable: true})
 cardName: string;
 
-@Column("text",{nullable: true})
-serial: string;
+@Column("integer",{nullable: true})
+serial: number;
 
 @Column("text",{nullable: true})
 type: string;
@@ -28,7 +28,7 @@ rarity: string;
 condition: string;
 
 @Column("text",{nullable: true})
-imageurl: string;
+imageUrl: string;
 
 
 }
