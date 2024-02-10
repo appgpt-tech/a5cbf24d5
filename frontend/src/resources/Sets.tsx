56353a40127b74270844292d77afd875cb23d0c2
11ai <124818466+11ai@users.noreply.github.com>
2024-02-10 11:05:27 +0200

    created on Sat Feb 10 2024 11:05:08 GMT+0200 (Eastern European Standard Time)

---
+++ frontend/src/resources/Sets.tsx
@@ -0,0 +1,100 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  UrlInput,
+  PasswordInput
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ReadOnlyPasswordField = ({ record, source }) => {
+
+  // You can customize the way you display the password here, e.g., mask it with asterisks
+  const maskedPassword =  '********';
+
+  return (
+      <span>{maskedPassword}</span>
+  );
+};
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const SetsTitle = () => {
+  const record = useRecordContext();
+  return <span>Sets {record ? `"${ record.setId }"` : ""}</span>;
+};
+
+export const SetsList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="setId" />
+<TextField source="setname" />
+<DateField source="releasedate" />
+<NumberField source="totalcards" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const SetsEdit = () => (
+                    <Edit title={<SetsTitle />}>
+                      <SimpleForm>
+                          <TextInput source="setId"   />
+<TextInput source="setname"   />
+<DateInput source="releasedate"   />
+<NumberInput source="totalcards"   />
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const SetsCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="setId"   />
+<TextInput source="setname"   />
+<DateInput source="releasedate"   />
+<NumberInput source="totalcards"   />
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+,
+,
+
+    ];
+
+
