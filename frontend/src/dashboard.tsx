f872b12aa146b52151527236a78995230e856ffb
11ai <124818466+11ai@users.noreply.github.com>
2024-02-12 22:40:34 +0200

    edited on Mon Feb 12 2024 22:40:24 GMT+0200 (Eastern European Standard Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -2,6 +2,6 @@ import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
       <CardHeader title= "ShopSphere Backoffice" />
-      <CardContent> an ecommerce backoofice system to support administration needs of a frontend ecommerce website that provides a seamless shopping experience for users worldwide. < /CardContent>
+      <CardContent> an ecommerce backoffice system to support administration needs of a frontend ecommerce website that provides a seamless shopping experience for users worldwide. < /CardContent>
       </Card>
     );
