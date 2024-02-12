1d42eac6d6087818aad4464011fa94390250b142
11ai <124818466+11ai@users.noreply.github.com>
2024-02-12 22:40:53 +0200

    edited on Mon Feb 12 2024 22:40:25 GMT+0200 (Eastern European Standard Time)

--- frontend/src/resources/SupportTickets.tsx
+++ frontend/src/resources/SupportTickets.tsx
@@ -57,8 +57,8 @@ const SupportTicketsTitle = () => {
 export const SupportTicketsList = () => (
       <List actions={<ListActions  />} filters={ResourceFilters} >
         <DatagridConfigurable>
-          <ReferenceField source="userId" reference="users"  />
-<ReferenceField source="customerId" reference="customers"  />
+          <ReferenceField source="user" reference="Users"  />
+<ReferenceField source="customer" reference="Customers"  />
 
 <TextField source="status" />
 <DateField source="creationDate" />
@@ -71,8 +71,8 @@ export const SupportTicketsList = () => (
 export const SupportTicketsEdit = () => (
                     <Edit title={<SupportTicketsTitle />}>
                       <SimpleForm>
-                          <ReferenceInput source="userId"  reference="users"   />
-<ReferenceInput source="customerId"  reference="customers"   />
+                          <ReferenceInput source="user"  reference="Users"   />
+<ReferenceInput source="customer"  reference="Customers"   />
 <TextInput source="description"   />
 <TextInput source="status"   />
 <DateInput source="creationDate"   />
@@ -84,8 +84,8 @@ export const SupportTicketsEdit = () => (
 export const SupportTicketsCreate = () => (
                                   <Create>
                                     <SimpleForm>
-                                        <ReferenceInput source="userId"  reference="users"   />
-<ReferenceInput source="customerId"  reference="customers"   />
+                                        <ReferenceInput source="user"  reference="Users"   />
+<ReferenceInput source="customer"  reference="Customers"   />
 <TextInput source="description"   />
 <TextInput source="status"   />
 <DateInput source="creationDate"   />
@@ -96,9 +96,9 @@ export const SupportTicketsCreate = () => (
 
 const ResourceFilters = [
       <TextInput source="q" label="Search" alwaysOn />,
+<ReferenceInput source="user" label="user" reference="Users"   alwaysOn/>,
+<ReferenceInput source="customer" label="customer" reference="Customers"   alwaysOn/>,
 ,
-<ReferenceInput source="userId" label="userId" reference="users"   alwaysOn/>,
-<ReferenceInput source="customerId" label="customerId" reference="customers"   alwaysOn/>,
 ,
 ,
 ,
