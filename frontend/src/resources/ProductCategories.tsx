64f5eeca2c37e2271dc01abe030184cd881fdd54
11ai <124818466+11ai@users.noreply.github.com>
2024-02-12 22:26:44 +0200

    created on Mon Feb 12 2024 22:26:22 GMT+0200 (Eastern European Standard Time)

---
+++ frontend/src/resources/ProductCategories.tsx
@@ -0,0 +1,88 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  UrlInput,
+  PasswordInput
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ReadOnlyPasswordField = ({ record, source }) => {
+
+  // You can customize the way you display the password here, e.g., mask it with asterisks
+  const maskedPassword =  '********';
+
+  return (
+      <span>{maskedPassword}</span>
+  );
+};
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const ProductCategoriesTitle = () => {
+  const record = useRecordContext();
+  return <span>ProductCategories {record ? `"${ record.id }"` : ""}</span>;
+};
+
+export const ProductCategoriesList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="description" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const ProductCategoriesEdit = () => (
+                    <Edit title={<ProductCategoriesTitle />}>
+                      <SimpleForm>
+                          <TextInput source="description"   />
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const ProductCategoriesCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="description"   />
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+
+    ];
+
+
